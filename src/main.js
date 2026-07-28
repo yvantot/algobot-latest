1dfb804da14db85cfde301b78aaa4ef6bd622e9c
yvantot <jidalman.work@gmail.com>
2026-07-29 06:32:08 +0800

    Serve JS interpreter as static script

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import { mount } from "svelte";
 import "./index.css";
-// JS-Interpreter is a legacy global script. Import it through Vite so it is
-// bundled for production instead of being requested from the source directory.
-import "./lib/js-interpreter.js";
 import App from "./App.svelte";
 
 const app = mount(App, {
